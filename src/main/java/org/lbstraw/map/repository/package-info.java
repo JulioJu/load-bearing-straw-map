5496e4bb63b03b8939a4243885381c0d98beed13
JulioJu <11527844+JulioJu@users.noreply.github.com>
2021-10-03 07:22:57 +0200

    Initial version of lbstrawmap generated by generator-jhipster@7.2.0

---
+++ src/main/java/org/lbstraw/map/repository/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Spring Data JPA repositories.
+ */
+package org.lbstraw.map.repository;
---
+++ src/test/java/org/lbstraw/map/IntegrationTest.java
@@ -0,0 +1,20 @@
+package org.lbstraw.map;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.lbstraw.map.LbstrawmapApp;
+import org.lbstraw.map.ReactiveSqlTestContainerExtension;
+import org.springframework.boot.test.context.SpringBootTest;
+
+/**
+ * Base composite annotation for integration tests.
+ */
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.RUNTIME)
+@SpringBootTest(classes = LbstrawmapApp.class)
+@ExtendWith(ReactiveSqlTestContainerExtension.class)
+public @interface IntegrationTest {
+}
