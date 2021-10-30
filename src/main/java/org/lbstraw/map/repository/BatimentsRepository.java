0000840e1b81c7bce7b6d81180fa46cfea875ccf
JulioJu <11527844+JulioJu@users.noreply.github.com>
2021-10-30 14:01:00 +0200

    feat: enum data-model.jdl, add utf8 value

--- src/main/java/org/lbstraw/map/repository/BatimentsRepository.java
+++ src/main/java/org/lbstraw/map/repository/BatimentsRepository.java
@@ -1,5 +1,6 @@
 package org.lbstraw.map.repository;
 
+// START added by JulioJu
 import java.util.List;
 import org.lbstraw.map.domain.Batiments;
 import org.lbstraw.map.domain.BatimentsLazyView;
@@ -12,8 +13,7 @@ import org.springframework.stereotype.Repository;
 @SuppressWarnings("unused")
 @Repository
 public interface BatimentsRepository extends JpaRepository<Batiments, Long> {
-    // START added by JulioJu
     @Query("select ba.id as id, ba.nom as nom, ba.latitude as latitude, ba.longitude as longitude from Batiments ba")
     List<BatimentsLazyView> findAllLazy();
-    // END added by JulioJu
 }
+// END added by JulioJu
