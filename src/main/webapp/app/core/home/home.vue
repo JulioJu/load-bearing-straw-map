90e2e6f72353e815fe5c5e31e192abfcc5fe8fe1
JulioJu <11527844+JulioJu@users.noreply.github.com>
2021-11-13 20:08:41 +0100

    feat: add waring on home page, site under constrution

--- src/main/webapp/app/core/home/home.vue
+++ src/main/webapp/app/core/home/home.vue
@@ -1,6 +1,11 @@
 <template>
   <div class="home">
     <!-- START added by JulioJu -->
+    <div class="mb-3">
+      <strong class="text-danger">
+        SITE EN COURS DE CONSTRUCTION, LES DONNÉES PEUVENT ÊTRE SUPPRIMÉES. Merci de ne rien enregistrer, hormis à des fins de test.
+      </strong>
+    </div>
     <map-container />
     <!-- END added by JulioJu -->
   </div>
